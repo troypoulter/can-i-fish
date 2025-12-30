0db0bdce43eb51002163380192ab8fbc638bd37d
troypoulter <troypoulter@gmail.com>
2025-12-31 08:31:09 +1100

    support latest trigger version

--- trigger.config.ts
+++ trigger.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
 	project: "proj_ckfuophgjnqcbjiftqvp",
+	maxDuration: 300,
 	runtime: "node",
 	logLevel: "info",
 	// Set the maxDuration to 300 seconds for all tasks. See https://trigger.dev/docs/runs/max-duration
